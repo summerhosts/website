61da2e0cfb6585d2e134c7999da1cf07b4d182c3
CubeBeveled <cubebeveled@gmail.com>
2025-02-13 18:55:22 +0100

    what

---
+++ docusaurus.config.ts
@@ -0,0 +1,92 @@
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: 'Summerhosts',
+  tagline: 'The history of ephemeral hosting services.',
+  favicon: 'img/favicon.svg',
+
+  // Set the production url of your site here
+  url: 'https://summerhosts.netlify.app',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'summerhosts', // Usually your GitHub org/user name.
+  projectName: 'summerhosts-website', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          routeBasePath: '/', // Serve the docs at the site's root
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: ({ locale, docPath }) => {
+            return `https://github.com/summerhosts/website/edit/main/${docPath}`;
+          },
+          showLastUpdateAuthor: true,
+          showLastUpdateTime: true,
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'logo.svg',
+    navbar: {
+      title: 'Summerhosts',
+      logo: {
+        alt: 'Site logo',
+        src: 'img/favicon.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Tutorial',
+        },
+        {
+          href: 'https://github.com/facebook/docusaurus',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [],
+      copyright: `Copyright © ${new Date().getFullYear()} Summerhosts, Inc. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
