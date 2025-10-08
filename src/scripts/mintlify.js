55fd747706f56320b5753dd6545a110d291412d3
CubeBeveled <cubebeveled@gmail.com>
2025-10-08 14:39:15 +0200

    mintlify shiz

---
+++ src/scripts/mintlify.js
@@ -0,0 +1,65 @@
+const fs = require("node:fs");
+const path = require("node:path");
+let docsTab = {
+  tab: "Documentation",
+  pages: [],
+  groups: [],
+};
+
+for (const f of fs.readdirSync("../../docs", { withFileTypes: true })) {
+  if (f.isDirectory()) {
+    docsTab.groups.push({
+      group: path.basename(f.name),
+      pages: exploreGroup(path.join("../../docs", f.name)),
+    });
+  } else docsTab.pages.push(f.name);
+}
+
+function exploreGroup(dir) {
+  let pages = [];
+
+  for (const f of fs.readdirSync(dir, { withFileTypes: true })) {
+    if (f.isDirectory()) {
+      pages.push({
+        group: path.basename(f.name),
+        pages: exploreGroup(path.join(dir, f.name)),
+      });
+    } else pages.push(removeExt(f.name));
+  }
+
+  return pages;
+}
+
+function removeExt(name) {
+  let a = name.split(".");
+  a.pop();
+  return a.join(".");
+}
+
+fs.writeFileSync(
+  "../../docs/docs.json",
+  JSON.stringify(
+    {
+      $schema: "https://mintlify.com/docs.json",
+      theme: "mint",
+      name: "Summerhosts",
+      colors: {
+        primary: "#17d6cc",
+      },
+      navigation: {
+        tabs: [docsTab],
+        global: {
+          anchors: [
+            {
+              anchor: "GitHub",
+              href: "https://github.com/summerhosts/website",
+              icon: "github",
+            },
+          ],
+        },
+      },
+    },
+    null,
+    2
+  )
+);
